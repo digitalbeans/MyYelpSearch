49f880bf6a6ded1b8108890914b517ce803cf660
Dean Thibault <deanthibault@digitalbeans.net>
2018-11-09 10:41:54 -0500

    Initial Commit

---
+++ MyYelpSearchTests/MyYelpSearchTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyYelpSearchTests.swift
+//  MyYelpSearchTests
+//
+//  Created by Dean Thibault on 11/9/18.
+//  Copyright © 2018 Digital Beans. All rights reserved.
+//
+
+import XCTest
+@testable import MyYelpSearch
+
+class MyYelpSearchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
